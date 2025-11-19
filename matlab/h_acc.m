c6450c4add500c782ecc708194caaad7a7518b77
Ozaslan <tozaslan@ybu.edu.tr>
2025-11-19 17:37:07 +0300

    Clarify accelerometer gravity transformation

--- matlab/h_acc.m
+++ matlab/h_acc.m
@@ -1,21 +1,23 @@
 function z_pred = h_acc(x, g)
 % H_ACC  Accelerometer measurement model
 %
-%   z_pred = (-g) * [2(qx qz + qy qw);
-%                    2(qy qz - qx qw);
-%                    1 - 2(qx^2 + qy^2)] + b_a
+% The accelerometer measures gravity expressed in the body frame plus the
+% accelerometer bias.  For a unit quaternion q that rotates vectors from the
+% body frame to the inertial frame, the rotation matrix R = quat_to_rotm(q)
+% has its third column equal to
+%
+%   R(:,3) = [2(qx*qz + qy*qw);
+%             2(qy*qz - qx*qw);
+%             1 - 2*(qx^2 + qy^2)].
+%
+% Therefore z_pred = g * R(:,3) + b_a.
 
 try
     q  = x(1:4);
     ba = x(8:10);
 
-    qw = q(1); qx = q(2); qy = q(3); qz = q(4);
-
-    hx = (g) * 2 * (qx*qz + qy*qw);
-    hy = (g) * 2 * (qy*qz - qx*qw);
-    hz = (g) * (1 - 2*(qx^2 + qy^2));
-
-    z_pred = [hx; hy; hz] + ba;
+    R = quat_to_rotm(q);
+    z_pred = g * R(:,3) + ba;
 
     if any(isnan(z_pred(:)))
         error('h_acc produced NaN values.');
